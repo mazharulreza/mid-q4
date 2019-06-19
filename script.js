798bd0ad35b0748d50a75575d46bc8cf6e1b918d
Reza <mazharulislamreza@gmail.com>
2019-06-19 22:07:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 /* Add your script here */
+var input1 = getElementByID("myInput1");
+var input2 = getElementByID("myInput2");
+$(document).ready(function () {
+    $('roundBtn').click(function (
+        $('#inp').text(input1 + input2).;
+))
+});
+
